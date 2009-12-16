fdac48c26480010d84e0a7e7d8c76a7a959c4263
Kevin A. Mitchell <kam@datalogics.com>
2009-12-16 17:39:00 -0600

    Fix scons.bat to work in a virtualenv.
    If it finds python.exe and scons adjacent to the batch file, it uses
    those. That is the situation when the .bat file, python.exe, and the
    virtualenv-generated scons script are all in the virtualenv\Scripts
    directory together.

--- src/script/scons.bat
+++ src/script/scons.bat
@@ -7,6 +7,15 @@ python -c "from os.path import join; import sys; sys.path = [ join(sys.prefix, '
 @REM no way to set exit status of this script for 9x/Me
 goto endscons
 :WinNT
+if NOT EXIST "%~dp0python.exe" goto novirtualenv
+if NOT EXIST "%~dp0scons" goto novirtualenv
+"%~dp0python" "%~dp0scons" %*
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endscons
+if errorlevel 9009 echo you do not have python in your PATH
+@REM color 00 causes this script to exit with non-zero exit status
+if errorlevel 1 color 00
+goto endscons
+:novirtualenv
 @REM set path=%path%;%~dp0
 python -c "from os.path import join; import sys; sys.path = [ join(sys.prefix, 'Lib', 'site-packages', 'scons-__VERSION__'), join(sys.prefix, 'Lib', 'site-packages', 'scons'), join(sys.prefix, 'scons-__VERSION__'), join(sys.prefix, 'scons')] + sys.path; import SCons.Script; SCons.Script.main()" %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endscons
