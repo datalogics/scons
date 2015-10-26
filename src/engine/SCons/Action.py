d2f5a8c8e6dbabffc600062aaec3c2c8b6958e2a
Jeff Harmon <jeffh@datalogics.com>
2015-10-26 10:02:14 -0700

    Adding logic to use PowerShell when available and necessary to avoid command line length limits in Windows. This repairs a fatal build error in DLE.

--- src/engine/SCons/Action.py
+++ src/engine/SCons/Action.py
@@ -477,6 +477,49 @@ class CommandAction(_ActionAction):
 
         cmd_list, ignore, silent = self.process(target, map(rfile, source), env)
 
+        # DLADD: JRH 20151022 SF37871/SF37872: DLE now contains to many .java and .cs files
+        #                                      to compile with the standard windows cmd.exe
+        # on windows, use powershell when available/necessary (to avoid command line length limit failures)
+        if sys.platform == 'win32':
+            try:
+                maxline = int(env.subst('$MAXLINELENGTH'))
+            except ValueError:
+                maxline = 2048
+
+            psc_key = None
+            try:
+                if sys.version_info < (3,0):
+                    import _winreg as wreg
+                else:
+                    import winreg as wreg
+
+                psc_key = wreg.OpenKey(wreg.HKEY_CLASSES_ROOT, 'Microsoft.PowerShellConsole.1')
+                psc_value = wreg.QueryValueEx(psc_key, 'FriendlyTypeName')
+
+                # the parse logic (below) might be a bit sketchy since it implies a schema
+                # for the registry key. if that schema should change, this logic will fail
+                # to tease the path out of the registry key's raw value. at present, the
+                # raw value is a tuple that contains a compound, "tuple-like" string.
+                raw_path = psc_value[0].split('"')[1]
+
+                if '%systemroot%' in raw_path:
+                    powershell_path = string.replace(raw_path, '%systemroot%', os.getenv('systemroot'))
+
+                if os.path.isfile(powershell_path) == True:
+                    maxline -= (len(cmd_line) - 1) # account for command line whitespace
+                    for item in cmd_line:
+                        maxline -= len(item)
+                        if maxline < 0:
+                            # we're facing a 'command line to long error'
+                            # so, use powershell instead...
+                            shell = powershell_path
+                            break
+            except:
+                pass
+            finally:
+                if psc_key != None:
+                    wreg.CloseKey(psc_key)
+
         # Use len() to filter out any "command" that's zero-length.
         for cmd_line in filter(len, cmd_list):
             # Escape the command line for the interpreter we are using.
